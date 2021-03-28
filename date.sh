5794305016074d0e0bd02ef622cacd21c4423b4b
yordi-16 <yordilinda24@gmail.com>
2021-03-28 16:37:16 -0400

    date

---
+++ date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATE=$(date)
+DATE1=date
+
+
+echo "=============================="
+
+echo "Today's date is $(date)"
+echo "Today's date is $DATE"
+
+echo "============================="
