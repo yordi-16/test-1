f94523423f527ba59566b05e6029da473eaee3b7
yordi-16 <yordilinda24@gmail.com>
2021-03-27 20:27:19 -0400

    color

---
+++ yordi1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# USING ANSI COLORS
+
+RED="\e[31m"
+ORANGE="\e[33m"
+BLUE="\e[94m"
+GREEN="\e[92m"
+STOP="\e[0m"
+
+printf "${GREEN}"
+printf "======================================================\n"
+
+printf "${ORANGE}"
+figlet -w 200 -f standard "LOVE"
+printf "${BLUE}"
+figlet -w 200 -f small "FAITH AND HOPE"
+printf "${GREEN}"
+
+printf "=====================================================\n"
+printf "${STOP}"
